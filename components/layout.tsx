3343f106e42765ea3bfb5d934bbb476ff75c2b17
JLarky <jlarky@gmail.com>
2021-01-07 01:47:38 -0800

    remove space

--- components/layout.tsx
+++ components/layout.tsx
@@ -157,7 +157,7 @@ export function Layout({
       <footer className="bg-white border-t border-gray-400 shadow">
         <div className="container max-w-4xl mx-auto flex py-8">
           <div className="w-full mx-auto flex flex-wrap">
-            <div className="flex w-full md:w-1/2 ">
+            <div className="flex w-full md:w-1/2">
               <div className="px-8">
                 <h3 className="font-bold text-gray-900">About</h3>
                 <p className="py-4 text-gray-600 text-sm">
